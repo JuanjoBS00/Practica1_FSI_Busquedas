84aeb8a0ba7d9a08204f7c1931dd30a9a9d272de
JuanjoBS00 <byjuanjo2000@gmail.com>
2021-03-23 22:26:37 +0000

    Segunda entrega con heurística añadida v3

--- run.py
+++ run.py
@@ -13,12 +13,6 @@ sn = search.GPSProblem('S', 'N'
 lg = search.GPSProblem('L', 'G'
                        , search.romania)
 
-#print(search.breadth_first_graph_search(ab).path())
-#print(search.depth_first_graph_search(ab).path())
-#print(search.branch_and_bound_search(ab).path())
-#print(search.branch_and_bound_subestimation_search(ab).path())
-
-
 print("Traslado de Arad a Bucarest")
 print("\nBúsqueda en anchura")
 nodeBFS, visitedNodesBFS = search.breadth_first_graph_search(ab)
@@ -111,14 +105,3 @@ nodeBB, visitedNodesBB = search.branch_and_bound_search(lg)
 nodeBBS, visitedNodesBBS = search.branch_and_bound_subestimation_search(lg)
 print("Sin subestimación: " + str(nodeBB.path()) + "\nCon subestimación: " + str(nodeBBS.path()))
 print("Sin subestimación: " + str(visitedNodesBB) + "\nCon subestimación: " + str(visitedNodesBBS))
-
-
-#Recorrido de Arad a Bucarest
-# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
-# DFS
-# [<Node B>, <Node P>, <Node C>, <Node D>, <Node M>, <Node L>, <Node T>, <Node A>] : 101 + 138 + 120 + 75 + 70 + 111 + 118 = 733
-# BNB
-# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
-
-#Parte 2. Tomar 5 pares de ciudades y comprobar que uno visitan menos nodos que otro.
-# Los mismos nodos son el resultado final pero uno visita mucho menos nodos que otro
